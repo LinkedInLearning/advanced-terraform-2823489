cdb6089289284e723e33603418d9ba8d3fd0e4eb
ahmed lotfy <ahmedlotfy700@msn.com>
2021-08-19 14:12:28 -0700

    ttest

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -15,7 +15,7 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 # //////////////////////////////
