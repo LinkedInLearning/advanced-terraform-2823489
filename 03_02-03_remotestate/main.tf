735998005c615583e83eb4ba7507ecc02f6261ef
ahmed lotfy <ahmedlotfy700@msn.com>
2021-08-19 15:09:04 -0700

    all to us east 2

--- 03_02-03_remotestate/main.tf
+++ 03_02-03_remotestate/main.tf
@@ -14,7 +14,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "vpc_cidr" {
@@ -45,7 +45,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
+  azs             = ["us-east-2a"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
