c98bd2dfbe97a3696d7edd05e3dc3a935ca03699
ahmed lotfy <ahmedlotfy700@msn.com>
2021-08-20 09:26:25 -0700

    hi there

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -11,7 +11,7 @@ variable "bucket_name" {
 
 # //////////////////////////////
 # PROVIDERs
-# //////////////////////////////
+# ////////////////
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
