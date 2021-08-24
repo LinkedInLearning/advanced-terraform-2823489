1463f3fe480c12cce598e3ec7df830b245bedfa0
ahmed lotfy <ahmedlotfy700@msn.com>
2021-08-24 16:18:04 -0700

    asd

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -56,7 +56,7 @@ EOF
 }
 
 resource "aws_s3_bucket_public_access_block" "red824-tfstate" {
-  bucket = aws_s3_bucket.red30-tfremotestate.id
+  bucket = aws_s3_bucket.red824-tfstate.id
 
   block_public_acls   = true
   block_public_policy = true
