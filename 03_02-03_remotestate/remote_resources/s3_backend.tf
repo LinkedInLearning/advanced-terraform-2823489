0414d52266ce79b43d8003bb2de41f98837eb5db
ahmed lotfy <ahmedlotfy700@msn.com>
2021-08-24 14:55:08 -0700

    ad:W!

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "bucket_name" {
-  default = "red-tfstate"
+  default = "red824-tfstate"
 }
 
 # //////////////////////////////
@@ -15,7 +15,7 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-2"
+  region     = "us-east-2"
 }
 
 # //////////////////////////////
@@ -29,9 +29,9 @@ data "aws_iam_user" "terraform" {
 # S3 BUCKET
 # //////////////////////////////
 resource "aws_s3_bucket" "red30-tfremotestate" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
-  acl = "private"
+  acl           = "private"
   versioning {
     enabled = true
   }
@@ -58,9 +58,9 @@ EOF
 resource "aws_s3_bucket_public_access_block" "red30-tfremotestate" {
   bucket = aws_s3_bucket.red30-tfremotestate.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -83,8 +83,8 @@ resource "aws_dynamodb_table" "tf_db_statelock" {
 # IAM POLICY
 # //////////////////////////////
 resource "aws_iam_user_policy" "terraform_user_dbtable" {
-  name = "terraform"
-  user = data.aws_iam_user.terraform.user_name
+  name   = "terraform"
+  user   = data.aws_iam_user.terraform.user_name
   policy = <<EOF
 {
     "Version": "2012-10-17",
