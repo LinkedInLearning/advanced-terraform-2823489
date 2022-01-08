7049e4aa73e4172584f04fc4c9f01511598f0cf7
chri carelli <50923511+chris-carelli@users.noreply.github.com>
2022-01-08 16:09:18 -0500

    set environment tag to staging

--- 02_10_count/main.tf
+++ 02_10_count/main.tf
@@ -143,8 +143,8 @@ resource "aws_security_group" "sg-nodejs-instance" {
 }
 
 # INSTANCE
-resource "aws_instance" "nodejs1" {
-  //count = 4
+resource "aws_instance" "node_instances" {
+  count = 4
 
   ami = data.aws_ami.aws-linux.id
   instance_type = var.environment_instance_settings["PROD"].instance_type
@@ -156,6 +156,11 @@ resource "aws_instance" "nodejs1" {
   tags = {Environment = var.environment_list[0]}
 }
 
+resource "aws_iam_user" "iam-users" {
+  for_each = var.iam_accounts
+
+  name = each.key
+}
 
 # //////////////////////////////
 # DATA
@@ -186,5 +191,5 @@ data "aws_ami" "aws-linux" {
 # OUTPUT
 # //////////////////////////////
 output "instance-dns" {
-  value = aws_instance.nodejs1.public_dns
+  value = aws_instance.node_instances.*.public_dns
 }
