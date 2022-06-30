b9582f7b7db95cac28d8e65a5cfa9084171efe5d
Connie Truong <truongc2@vcu.edu>
2022-06-30 13:39:15 -0400

    set environment tag to staging

--- 02_01_variables/main.tf
+++ 02_01_variables/main.tf
@@ -5,6 +5,10 @@ variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
 
+variable "deploy_environment" {
+  default = "DEV"
+}
+
 variable "region" {
   default = "us-east-2"
 }
@@ -141,14 +145,14 @@ resource "aws_security_group" "sg-nodejs-instance" {
 # INSTANCE
 resource "aws_instance" "nodejs1" {
   ami = data.aws_ami.aws-linux.id
-  instance_type = var.environment_instance_type["DEV"]
-  //instance_type = var.environment_instance_settings["PROD"].instance_type
+  //instance_type = var.environment_instance_type["DEV"]
+  instance_type = var.environment_instance_settings[var.deploy_environment].instance_type
   subnet_id = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg-nodejs-instance.id]
 
-  monitoring = var.environment_instance_settings["PROD"].monitoring
+  monitoring = var.environment_instance_settings[var.deploy_environment].monitoring
 
-  tags = {Environment = var.environment_list[0]}
+  tags = {Environment = var.environment_map[var.deploy_environment]}
 
 }
 
